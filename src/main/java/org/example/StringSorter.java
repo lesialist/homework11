1cf02642b6d0f568e739aab8e2de1748e1915f49
smagles <lesialist@icloud.com>
2023-06-04 11:22:38 +0200

    Initial commit

---
+++ src/main/java/org/example/StringSorter.java
@@ -0,0 +1,15 @@
+package org.example;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StringSorter {
+    public List<String> getDescendingUpperCaseSortedStrings(List<String> list) {
+        return list.stream()
+                .map(s -> s.toUpperCase())
+                .sorted(Comparator.reverseOrder())
+                .collect(Collectors.toList());
+
+    }
+}
